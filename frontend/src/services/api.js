35ac5077942fc7faa48e5bfec6c1ad9a9f01f19b
Nikhil Anand <nikhilanand2202@gmail.com>
2026-01-18 01:29:04 +0530

    fix: update API base URL to use HTTPS for secure connections

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
  // baseURL: "http://localhost:5000/api",
-  baseURL: "http://storeratings-production.up.railway.app/api",
+  baseURL: "https://storeratings-production.up.railway.app/api",
 });
 
 export default api;
